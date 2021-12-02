97ecf667e3f2290ffb9eb6bad5cb95bce8716ead
TaLea Monet <taleamonet@gmail.com>
2021-12-02 15:22:20 -0800

    experimenting with event listeners, etc.

--- popup.js
+++ popup.js
@@ -1,5 +1,22 @@
 let ccButton = document.getElementById("changeColor");
+let newButton = document.getElementById("newColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
   ccButton.style.backgroundColor = color;
 });
+
+
+ccButton.addEventListener("click", clickHandler);
+newButton.addEventListener("click", clickHandler);
+
+async function clickHandler(evt) {
+  // if(evt.x < 35){
+  //   console.log("click registered on the left half of", evt.target.id);
+  // }
+  // else {
+  //   console.log("click registered on the right half of", evt.target.id);
+  // }
+  //console.log("In click listener",evt.x, evt.y, evt.target.id);
+  let tabs = await chrome.tabs.query({});
+  console.log("tabs =", tabs);
+}
