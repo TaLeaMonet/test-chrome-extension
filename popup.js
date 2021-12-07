7805b8398e407de1ca65ef87226a733365710380
TaLea Monet <taleamonet@gmail.com>
2021-12-06 16:08:49 -0800

    Implement window count ability
    Also, refactored tab count to be conditional.

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 let ccButton = document.getElementById("changeColor");
-let newButton = document.getElementById("newColor");
+let tabButton = document.getElementById("tab-button");
 let tabCount = document.getElementById("tab-count");
+let windowButton = document.getElementById("window-button");
+let windowCount = document.getElementById("window-count");
 
 chrome.storage.sync.get("color", ({ color }) => {
   ccButton.style.backgroundColor = color;
@@ -8,8 +10,9 @@ chrome.storage.sync.get("color", ({ color }) => {
 
 
 ccButton.addEventListener("click", clickHandler);
-newButton.addEventListener("click", clickHandler);
 
+tabButton.addEventListener("click", clickHandler);
+windowButton.addEventListener("click", clickHandler);
 async function clickHandler(evt) {
   // if(evt.x < 35){
   //   console.log("click registered on the left half of", evt.target.id);
@@ -17,8 +20,23 @@ async function clickHandler(evt) {
   // else {
   //   console.log("click registered on the right half of", evt.target.id);
   // }
-  //console.log("In click listener",evt.x, evt.y, evt.target.id);
-  let tabs = await chrome.tabs.query({});
-  console.log("tabs =", tabs);
-  tabCount.innerHTML = "You have " + tabs.length + " tabs currently open.";
+  console.log("In click listener",evt.x, evt.y, evt.target.id);
+  if( evt.target.id === "tab-button" ) {
+    let tabs = await chrome.tabs.query({});
+    console.log("tabs =", tabs);
+
+    if( tabs.length >= 10 ) {
+      alert("You have " + tabs.length + " tabs currently open.");
+    }
+    tabCount.innerHTML = "There are a total of " + tabs.length +
+      " tabs currently open."
+  };
+  //Count total number of windows open
+  if( evt.target.id === "window-button" ) {
+    chrome.windows.getAll(function(windows) {
+      windowCount.innerHTML = "There are a total of " + windows.length +
+        " windows currently open."
+      //console.log("There are a total of " + windows.length + " windows currently open.");
+    });
+  }
 }
